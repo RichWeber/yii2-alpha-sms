e46f14ad8575bc9918a266996f0e27375f61b5ca
RichWeber <rbagatyi@gmail.com>
2014-08-31 12:40:13 +0300

    test params

--- AlphaSms.php
+++ AlphaSms.php
@@ -3,5 +3,15 @@ namespace richweber\yii\alpha\sms;
 
 class AlphaSms
 {
-    //
+    public $login = 'login';
+    public $password = 'password';
+
+    public function test()
+    {
+        echo '<br>';
+        echo $this->login;
+        echo '<br>';
+        echo $this->password;
+        echo '<br>';
+    }
 }
